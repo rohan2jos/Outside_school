bba1299ac72f27647638d35efc6dc21348785a3f
rohan2j <rohan2j@ccs.neu.edu>
2016-01-02 21:46:41 -0500

    ListView Example
    ** not including the entire structure **
    
    This is code I wrote while learning the Android ListView
    
    Basic code containing hardcoded array of String values and displayed to
    the ListView.
    
    Comments in the java code that explain what is being done

---
+++ Android/ListViewExample/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.rohanjoshi.listviewer;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ListView listView = (ListView)findViewById(R.id.list);
+
+        String[] values = {"Optimus Prime", "Starscreem", "Megatron", "Ratchet", "Galvatron",
+                            "Sideways", "Hot Shot", "Scavenger", "Bumblebee"};
+
+        // we got the list view from the xml file
+        // We then created a new array that holds the values that we are going to display in the
+        // list view
+        // now we generate a new adapter that interfaces the list view and the values
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                                        android.R.layout.simple_list_item_1,
+                                        android.R.id.text1,
+                                        values);
+        listView.setAdapter(arrayAdapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
